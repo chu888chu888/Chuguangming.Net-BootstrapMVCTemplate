c40d0947afaf15c0d54e6ea93fa5440e53264eab
chu888chu888 <chu888chu888@gmail.com>
2012-11-14 14:52:30 +0800

    完成瀑布流原型

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -14,7 +14,19 @@ namespace WebUI.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Title = "ASP.NET MVC 4 Web Template";
+            //ViewBag.UserID = this.HttpContext.User.Identity.Name;
+            ViewBag.UserID = "欢迎您使用友学视频系统";
+
+            Dictionary<string, string> Dictclassify = new Dictionary<string, string>();
+            Dictclassify.Add("1", "分享电影");
+            Dictclassify.Add("2", "好听音乐");
+            Dictclassify.Add("3", "好玩图片");
+            Dictclassify.Add("4", "分享图书");
+            Dictclassify.Add("5", "无限下载");
+            ViewBag.classify = Dictclassify;
+
+
+
             return View();
         }
         public ActionResult Detail(int id)
